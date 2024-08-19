dd97aaf957700ef752585290bc11194fda17ae81
Sohilahamdy <sohila.fathy22@gmail.com>
2024-08-19 16:36:54 -0700

    0x16. API advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,16 +5,22 @@ Contains the recurse function
 
 import requests
 
+
 def recurse(subreddit, hot_list=[]):
     """Returns a list of all hot articles for a given subreddit"""
     if not isinstance(subreddit, str) or not subreddit:
         return None
-    
+
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/your_username)'}
-    
+    headers = {
+        'User-Agent': (
+             '0x16-api_advanced:project:v1.0.0 (by /u/your_username)'
+        )
+    }
+
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         try:
             data = response.json().get('data', {})
@@ -22,12 +28,17 @@ def recurse(subreddit, hot_list=[]):
             if posts:
                 for post in posts:
                     hot_list.append(post.get('data', {}).get('title', ''))
-                
+
                 # Get the next page
                 after = data.get('after', None)
                 if after:
-                    next_url = f'https://www.reddit.com/r/{subreddit}/hot.json?after={after}'
-                    return recurse(subreddit, hot_list)  # Recursive call with updated hot_list
+                    next_url = (
+                        f'https://www.reddit.com/r/{subreddit}/hot.json'
+                        f'?after={after}'
+                    )
+
+                    # Recursive call with updated hot_list
+                    return recurse(subreddit, hot_list)
                 else:
                     return hot_list
             else:
